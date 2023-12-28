bfcfbbb61587ba038c15b1fee1c4add23e08cd7b
MohitMehta27 <Mohit20178@gmail.com>
2023-12-28 21:03:00 +0700

    Ingestion and transformation

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("C:\HousePrice")
+sys.path.append("C:/HousePrice")
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
@@ -21,15 +21,5 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message    
     
-
-
-if __name__=="__main__":
-    logging.info("Logging has started")
-
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info('Decision by zero') 
-        raise CustomException(e,sys)
 
 
