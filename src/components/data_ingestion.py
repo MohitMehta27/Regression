502147afcd6d393dfa657bbbaa4c570247e4b2d8
MohitMehta27 <Mohit20178@gmail.com>
2023-12-30 00:00:58 +0700

    Model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -46,8 +46,3 @@ class DataIngestion:
             logging.error(error_message) 
             raise CustomException(error_message, sys)
 
-if __name__ == "__main__":
-   obj=DataIngestion()
-   train_data_path,train_data_path=obj.initiate_data_ingestion()
-   data_transformation=DataTransformation()
-   train_arr, test_arr,_=data_transformation.initiate_data_transformation(train_data_path,train_data_path)
